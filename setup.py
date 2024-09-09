4af5f123eb7781e6208d529cfcba6ff19f1aed54
Mariusz Woloszyn <emsi@users.noreply.github.com>
2024-09-09 15:41:07 +0200

    make installable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='librus_pyapi',
+    version='',
+    packages=['librus_pyapi', 'librus_pyapi.schemas'],
+    url='',
+    license='GNU AFFERO GENERAL PUBLIC LICENSE',
+    author='emsi',
+    author_email='',
+    description=''
+)
